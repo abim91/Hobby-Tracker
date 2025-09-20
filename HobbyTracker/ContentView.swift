68dd1d857a4ab2b91873aa34559b9c6de0816f4e
abim91 <53064345+abim91@users.noreply.github.com>
2025-09-20 12:21:59 -0400

    Added Hobby Listed Screen

--- HobbyTracker/ContentView.swift
+++ HobbyTracker/ContentView.swift
@@ -8,78 +8,73 @@
 import SwiftUI
 import CoreData
 
+var HobbyList: [String:String] = [
+    "Run": "🏃‍♂️",
+    "Read": "📚",
+    "Draw": "🎨",
+    "Cook": "🍳",
+    "Write": "✍️",
+    "Swim": "🏊‍♀️",
+    "Dance": "💃",
+    "Sing": "🎤",
+    "Travel": "✈️",
+    "Garden": "🌱",
+    "Play": "🎮"
+]
 struct ContentView: View {
+   
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var showAddItemView = false
+    
+    //    @FetchRequest(
+    //        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+    //        animation: .default)
+    //        animation: .default)
+    //    private var items: FetchedResults<Item>
+    
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        
+        ScrollView {
+            VStack{
+                HStack{
+                    Text("HOBBY TRACKER").bold()
+                    Button(action: {
+                            showAddItemView.toggle()}) {
+                            Label(" ", systemImage: "plus")
+                        
+                        }.bold(true).padding(.leading, 150)
+                    
+                }
+                Spacer()
+                VStack{
+                    ForEach(Array(HobbyList.keys), id: \.self) { item in
+                        HStack{
+                            Text(item).padding(20)
+                            Text(HobbyList[item] ?? "")
+                        }
+                        Divider()
+
                     }
+                    
                 }
-                .onDelete(perform: deleteItems)
+                
+                
+                
+                
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        }.sheet(isPresented: $showAddItemView, content: {FormView() })
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct FormView: View{
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
